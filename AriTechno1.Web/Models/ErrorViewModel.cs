729e28e913fe3a1516357240cee834456b2d5c79
yarencekemci <yarencekemci@gmail.com>
2026-05-12 21:29:00 +0300

    igtu dosyaları aktarıldı

--- AriTechno1.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AriTechno1.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
